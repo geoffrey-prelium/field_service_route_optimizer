ffd4cc3384756311203781a9440241fb58e44b04
geoffrey-prelium <geoffrey.lecluse45@gmail.com>
2026-03-27 14:53:02 +0100

    [FIX] Automatically suggest unassigned tasks in the wizard

--- wizard/add_route_tasks.py
+++ wizard/add_route_tasks.py
@@ -6,6 +6,16 @@ class AddRouteTasks(models.TransientModel):
 
     task_ids = fields.Many2many('project.task', string="Tasks", domain="[('route_id', '=', False)]")
 
+    @api.model
+    def default_get(self, fields_list):
+        """Automatically suggest tasks that are not yet on any route."""
+        res = super().default_get(fields_list)
+        if 'task_ids' in fields_list:
+            unassigned_tasks = self.env['project.task'].search([('route_id', '=', False)])
+            if unassigned_tasks:
+                res['task_ids'] = [(6, 0, unassigned_tasks.ids)]
+        return res
+
     def action_add_tasks(self):
         """Action to add selected tasks to the active route."""
         self.ensure_one()
